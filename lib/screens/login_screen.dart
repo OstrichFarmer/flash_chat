7309b0c9af26f0c6dd0ce9261dd9b5f176bd243a
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-10-08 18:41:57 +0100

    fixes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,63 +17,67 @@ class _LoginScreenState extends State<LoginScreen> {
   String? password;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email')),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password')),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                onpressed: () async {
+                  try {
+                    final returningUser =
+                        await _auth.signInWithEmailAndPassword(
+                            email: email!, password: password!);
+                    if (returningUser.user != null) {
+                      Navigator.pushNamed(
+                        context,
+                        ChatScreen.id,
+                      );
+                    }
+                  } catch (e) {}
                 },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your email')),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password')),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              onpressed: () async {
-                try {
-                  final returningUser = await _auth.signInWithEmailAndPassword(
-                      email: email!, password: password!);
-                  if (returningUser.user != null) {
-                    Navigator.pushNamed(
-                      context,
-                      ChatScreen.id,
-                    );
-                  }
-                } catch (e) {}
-              },
-              title: 'Log in ',
-              color: Colors.lightBlueAccent,
-            )
-          ],
+                title: 'Log in ',
+                color: Colors.lightBlueAccent,
+              )
+            ],
+          ),
         ),
       ),
     );
